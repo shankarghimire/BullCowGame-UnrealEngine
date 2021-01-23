9a679219bc9983229cb1988da06149a8fa0f55ef
Shankar Ghimire <ssghimire@gmail.com>
2021-01-22 20:52:35 -0500

    Adds initial level of C++ code logic to game

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,25 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    HiddenWord = TEXT("cake");
+    PrintLine(TEXT("Welcome to BullCow Game!!!"));
+    PrintLine(TEXT("Guess a 4 letter word !")); //Will be replaced by Magic Number
+    PrintLine(TEXT("Press 'Enter' key to continue : "));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
+    //FString HiddenWord = TEXT("cake"); //Later will be relocated to next place to make it global
+    //PrintLine(Input);
+  
+    if( HiddenWord == Input)
+    {
+        PrintLine(TEXT("You Win!"));
+    }
+    else
+    {
+        PrintLine(TEXT("Sorry! You Lose!"));
+    }
 
 }
